25ce0a2a10a665fa5f4696e0ebb8bcd6d5016a6d
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2019-09-27 13:00:28 +0100

    guita electric 3D

--- src/game.ts
+++ src/game.ts
@@ -11,13 +11,14 @@ import { Tempo } from "./modules/recorder/tempo";
 import { Recorder3D } from "./modules/recorder/recorder3D";
 import { DrumSet } from "./modules/instruments/drumset";
 import { DrumSetUI } from "./modules/ui_instruments/drumset_ui";
+import { GuitarElec, getGuitarElecNotes } from "./modules/instruments/guitar_elec";
 
-const land = new Land(trace, new Transform({
-  position: new Vector3(8.0, 0.0, 15.8),
-  rotation: Quaternion.Euler(0, 0 ,0),
-  scale: new Vector3(0.97, 0.97, 0.97)
-}));
-engine.addEntity(land.getEntity());
+// const land = new Land(trace, new Transform({
+//   position: new Vector3(8.0, 0.0, 15.8),
+//   rotation: Quaternion.Euler(0, 0 ,0),
+//   scale: new Vector3(0.97, 0.97, 0.97)
+// }));
+// engine.addEntity(land.getEntity());
 
 const soundHub = new SoundHub(trace);
 
@@ -27,61 +28,68 @@ const gameCanvas = new UICanvas();
 //   position: new Vector3(8.0, 0.0, 8.0)
 // }));
 
-const piano = new Piano(trace, soundHub, new Transform({
-  position: new Vector3(2, 0.3, 13.0),
-  rotation: Quaternion.Euler(0, -30 ,0),
-  scale: new Vector3(0.12, 0.2, 0.2)
-}));
-const pianoUI = new PianoUI(trace, gameCanvas);
-pianoUI.setSoundHub(soundHub);
-const play_piano = new PlayIt(trace, new Transform({
-  position: new Vector3(0.3,8.0,-1.9),
-  rotation: Quaternion.Euler(0,-90,19),
-  scale: new Vector3(0.3,1.0,4.5)
-}), piano.getEntity(), pianoUI);
-engine.addEntity(piano.getEntity());
+// const piano = new Piano(trace, soundHub, new Transform({
+//   position: new Vector3(2, 0.3, 13.0),
+//   rotation: Quaternion.Euler(0, -30 ,0),
+//   scale: new Vector3(0.12, 0.2, 0.2)
+// }));
+// const pianoUI = new PianoUI(trace, gameCanvas);
+// pianoUI.setSoundHub(soundHub);
+// const play_piano = new PlayIt(trace, new Transform({
+//   position: new Vector3(0.3,8.0,-1.9),
+//   rotation: Quaternion.Euler(0,-90,19),
+//   scale: new Vector3(0.3,1.0,4.5)
+// }), piano.getEntity(), pianoUI);
+// engine.addEntity(piano.getEntity());
 
-const bass = new Bass(trace, soundHub, new Transform({
-  position: new Vector3(6, 1.5, 12.0),
+// const bass = new Bass(trace, soundHub, new Transform({
+//   position: new Vector3(5.5, 1.5, 12.5),
+//   rotation: Quaternion.Euler(20, 80 ,70),
+//   scale: new Vector3(0.1,0.1,0.1)
+// }));
+// const bassUI = new BassUI(trace, gameCanvas);
+// bassUI.setSoundHub(soundHub);
+// const play_bass = new PlayIt(trace, new Transform({
+//   position: new Vector3(4.0,0.0,0.0),
+//   rotation: Quaternion.Euler(0,180,90),
+//   scale: new Vector3(1.0,1.0,5.0)
+// }), bass.getEntity(), bassUI);
+// engine.addEntity(bass.getEntity());
+
+// const tempo = new Tempo(trace, 100, 4, 4);
+// const recorder3d = new Recorder3D(trace, new Transform({
+//   position: new Vector3(13.5, 1.0, 13.0),
+//   rotation: Quaternion.Euler(0, 65 ,0),
+//   scale: new Vector3(0.2,0.2,0.2)
+// }));
+// const recorderUI = new RecorderUI(trace, gameCanvas, soundHub, tempo);
+// const play_recorder = new PlayIt(trace, new Transform({
+//   position: new Vector3(0.8,1.2,0.0),
+//   rotation: Quaternion.Euler(0,180,0),
+//   scale: new Vector3(0.4,0.4,2.0)
+// }), recorder3d.getEntity(), recorderUI);
+// engine.addEntity(recorder3d.getEntity());
+
+// const drumSet = new DrumSet(trace, soundHub, new Transform({
+//   position: new Vector3(9.0, 0.3, 13.0),
+//   rotation: Quaternion.Euler(0,180,0),
+//   scale: new Vector3(0.2, 0.2, 0.2)
+// }));
+// const drumSetUI = new DrumSetUI(trace, gameCanvas);
+// drumSetUI.setSoundHub(soundHub);
+// const play_drums = new PlayIt(trace, new Transform({
+//   position: new Vector3(0.0,9.0,-1.0),
+//   rotation: Quaternion.Euler(0,90,0),
+//   scale: new Vector3(1.0,1.0,5.0)
+// }), drumSet.getEntity(), drumSetUI);
+// engine.addEntity(drumSet.getEntity());
+
+const guitarElec = new GuitarElec(trace, soundHub, new Transform({
+  position: new Vector3(11.5, 1.8, 12.0),
   rotation: Quaternion.Euler(20, 80 ,70),
   scale: new Vector3(0.1,0.1,0.1)
 }));
-const bassUI = new BassUI(trace, gameCanvas);
-bassUI.setSoundHub(soundHub);
-const play_bass = new PlayIt(trace, new Transform({
-  position: new Vector3(4.0,0.0,0.0),
-  rotation: Quaternion.Euler(0,180,90),
-  scale: new Vector3(1.0,1.0,5.0)
-}), bass.getEntity(), bassUI);
-engine.addEntity(bass.getEntity());
-
-const tempo = new Tempo(trace, 100, 4, 4);
-const recorder3d = new Recorder3D(trace, new Transform({
-  position: new Vector3(13.0, 1.0, 13.0),
-  rotation: Quaternion.Euler(0, 65 ,0),
-  scale: new Vector3(0.2,0.2,0.2)
-}));
-const recorderUI = new RecorderUI(trace, gameCanvas, soundHub, tempo);
-const play_recorder = new PlayIt(trace, new Transform({
-  position: new Vector3(0.8,1.2,0.0),
-  rotation: Quaternion.Euler(0,180,0),
-  scale: new Vector3(0.4,0.4,2.0)
-}), recorder3d.getEntity(), recorderUI);
-engine.addEntity(recorder3d.getEntity());
-
-const drumSet = new DrumSet(trace, soundHub, new Transform({
-  position: new Vector3(9.0, 0.3, 13.0),
-  rotation: Quaternion.Euler(0,180,0),
-  scale: new Vector3(0.2, 0.2, 0.2)
-}));
-const drumSetUI = new DrumSetUI(trace, gameCanvas);
-drumSetUI.setSoundHub(soundHub);
-const play_drums = new PlayIt(trace, new Transform({
-  position: new Vector3(0.0,9.0,-1.0),
-  rotation: Quaternion.Euler(0,90,0),
-  scale: new Vector3(1.0,1.0,5.0)
-}), drumSet.getEntity(), drumSetUI);
-engine.addEntity(drumSet.getEntity());
+engine.addEntity(guitarElec.getEntity());
 // recorderUI.display();
 function trace (message) {
   console.log(message);
---
+++ src/modules/instruments/guitar_elec.ts
@@ -0,0 +1,94 @@
+import { Instrument, INoteProps } from "./Instrument";
+import { Note } from "./note";
+import { ISoundHub } from "../soundhub/soundhub";
+
+const guitarElecModelFile = "models/guitar_elec.gltf";
+const soundsPath = "sounds/guitar_elec/";
+const extension = ".wav";
+var guitarElecNotes: INoteProps[];
+export function getGuitarElecNotes(): INoteProps[] {
+    if (!guitarElecNotes) {
+        guitarElecNotes = [];
+        let index = 0;
+        let cord = 1;
+        let position = 0;
+        for (let octave of [2, 3, 4]) {
+            for (let letter of ["C", "D", "E", "F", "G", "A", "B"]) {
+                if ((octave === 2) && (["C", "D"].indexOf(letter) !== -1)) continue; // start with E2
+                let note = letter + octave;
+                if (((octave === 2) && (letter === "A"))  ||
+                    ((octave === 3) && (letter === "D"))  ||
+                    ((octave === 3) && (letter === "G"))  ||
+                    ((octave === 3) && (letter === "B"))  ||
+                    ((octave === 4) && (letter === "E"))
+                ) { // change cord
+                    guitarElecNotes.push({
+                        note: note + "_", index: index, song : soundsPath + note + extension, extras: {"cord": cord, "position": position}
+                    });
+                    cord++;
+                    position = 0;
+                }
+                guitarElecNotes.push({
+                    note: note, index: index, song : soundsPath + note + extension, extras: {"cord": cord, "position": position}
+                });
+                index++;
+                position++;
+                if ((octave === 4) && (letter === "A")) break; // end with A4
+                if (["A","C","D","F","G"].indexOf(letter) !== -1) {
+                    let note = letter + octave + "_sharp";
+                    guitarElecNotes.push({
+                        note: note, index: index, song : soundsPath + note + extension, extras: {"cord": cord, "position": position}
+                    });
+                    index++;
+                    position++;
+                }
+            }
+        }
+    }
+    return guitarElecNotes;
+}
+
+export class GuitarElecNote extends Note {
+    getShape() {
+        return new PlaneShape();
+    }
+    getMaterial() {
+        const material = new Material()
+        material.albedoTexture = new Texture("images/fingerprint2.png",{hasAlpha: true});
+        material.hasAlpha = true;
+        return material;
+    }
+    getNoteShape(noteProp: INoteProps) {
+        return null;
+    }
+    getInstrument() {
+        return "guitar_elec";
+    }
+    animate() {
+    }
+}
+
+export class GuitarElec extends Instrument<GuitarElecNote> {
+    constructor(log: (string )=> void, soundHub: ISoundHub, transform: Transform) {
+        super(log, soundHub, transform);
+        this.createNotes(soundHub, GuitarElecNote, getGuitarElecNotes());
+    }
+    getMaterial() {
+        return null;
+    }
+    getShape() {
+        return new GLTFShape(guitarElecModelFile);
+    }
+    getTransformForNote(noteProp: INoteProps): Transform {
+        let posX = -1.06;
+        let posY = 1.1;
+        let posZ = -5.7;
+        let noteCord = noteProp.extras['cord'] as number;
+        let notePosition = noteProp.extras['position'] as number;
+        return new Transform({
+            position: new Vector3(posX - 0.11*noteCord + 0.07*notePosition, posY, posZ + 0.54*notePosition - 0.04*noteCord),
+            rotation: Quaternion.Euler(90,0,-10),
+            scale: new Vector3(0.12, 0.5, 0.1),
+        })
+    }
+}
