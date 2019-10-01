9c7fd16e31240a458eda8e5ebfdf922489943b48
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2019-10-01 08:50:00 +0100

    set Production and allow deploy on preprod

--- src/game.ts
+++ src/game.ts
@@ -30,10 +30,10 @@ let features = {
 
 /////// TEST CONFIGURATION  ///////////
 
-// const isProduction = true;
-const isProduction = false;
+const isProduction = true;
+// const isProduction = false;
 // const isIntegration = true;
-const isIntegration = true;
+const isIntegration = false;
 //const complete = false;
 const complete = isProduction || isIntegration;
 
